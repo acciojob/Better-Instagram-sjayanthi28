a662f51da78f7b2ba4311076412bde78df65be8c
sjayanthi28 <sjayanthisjayanthi88@gmail.com>
2025-12-19 16:14:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 //your code here
+
+const images = document.querySelectorAll(".image");
+let draggedDiv = null;
+
+images.forEach(img => {
+  img.addEventListener("dragstart", dragStart);
+  img.addEventListener("dragover", dragOver);
+  img.addEventListener("drop", drop);
+});
+
+function dragStart() {
+  draggedDiv = this;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  e.preventDefault();
+  if (draggedDiv === this) return;
+
+  
+  const draggedBg = getComputedStyle(draggedDiv).backgroundImage;
+  const targetBg = getComputedStyle(this).backgroundImage;
+
+  
+  draggedDiv.style.backgroundImage = targetBg;
+  this.style.backgroundImage = draggedBg;
+}
